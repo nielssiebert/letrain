f7bfc1c48ec40afac4b82ce324bf4f04ec6a53a9
Good Knight <nielssiebertdev@gmail.com>
2022-02-17 00:17:11 +0100

    added cors header

--- middelware/app.py
+++ middelware/app.py
@@ -17,30 +17,40 @@ def index():
 @app.route('/valves')
 def get_valves():
     valves = Valve.objects().to_json()
-    return Response(valves, mimetype="application/json", status=200)
+    response = Response(valves, mimetype="application/json", status=200)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/valves', methods=['POST'])
 def add_valve():
     body =request.get_json()
     valve = Valve(**body).save()
     id = valve.id
-    return {'id': str(id)}, 200
+    response = Response({'id': str(id)}, status=200)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/valves/<id>', methods=['PUT'])
 def update_valve(id):
     body = request.get_json()
     Valve.objects.get(id=id).update(**body)
-    return '', 200
+    response = Response({'id': str(id)}, status=200)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/valve/<id>', methods=['DELETE'])
 def delete_valve(id):
     Valve.objects.get(id=id).delete()
-    return '', 200
+    response = Response({'id': str(id)}, status=200)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/valves/<id>')
 def get_valve(id):
     valves = Valve.objects.get(id=id).to_json()
-    return Response(valves, mimetype="application/json", status=200)
+    response = Response(valves, mimetype="application/json", status=200)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == "__main__":
     from waitress import serve
